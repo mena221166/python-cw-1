d3c9d9e45f9d40b906d6e162251fd27c2e061112
mena221166 <menaesam2211@gmail.com>
2023-08-27 17:41:15 +0300

    mena.cw1
     first class work

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from traceback import print_exc
-print=("mena esam")
-num1=3
-num2=5
-print=(num2+num1)
+
+print("mena esam")
+num1=4
+num2=4
+num3=num1+num2
+print(num2+num1)
+print(num1%num2)
+print(num1*num2)
+
+length=5
+width=3
+area=length*width
+print (f"the result is {area}")
+
+print(f"if the length of the rectangle is {length} and width is {width } then the area is {area}")
 
 # Change the following print statement so that it uses the addition operator + and prints out 8
 
